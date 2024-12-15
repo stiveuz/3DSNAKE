65bbac668c85e211fb860a6abc6007755c2a4314
stiveuz <124035169+stiveuz@users.noreply.github.com>
2024-12-15 13:24:15 +0500

    1st level

---
+++ Assets/Scripts/Helper Scripts/AudioManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    public AudioClip pickUp_Sound, dead_Sound;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        MakeInstance();
+    }
+
+    void MakeInstance()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void Play_PickUpSound()
+    {
+        AudioSource.PlayClipAtPoint(pickUp_Sound, transform.position);
+    }
+
+    public void Play_DeadSound()
+    {
+        AudioSource.PlayClipAtPoint(dead_Sound, transform.position);
+    }
+}
---
+++ Assets/Scripts/Pickup Scripts/DeactivatePickUp.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactivatePickUp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("Deactivate", Random.Range(3f, 6f));
+    }
+
+    // Update is called once per frame
+    void Deactivate()
+    {
+        gameObject.SetActive(false);
+    }
+}
